63a0ddf9cc09c840575ca42901fe1e214b8b4e7a
ragezor <457974608@qq.com>
2020-02-14 17:15:51 +0800

    feat: the code of leetcode

--- src/byteDancing/RestoreIp.java
+++ src/byteDancing/RestoreIp.java
@@ -1,6 +1,32 @@
+package byteDancing;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+/*
+题干
+给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+
+示例:
+
+输入: "25525511135"
+输出: ["255.255.11.135", "255.255.111.35"]
+
+想法
+这题乍一看很唬人，仔细看其实就是要返回这个string的几种划分情况，并且一定是划分成四块，同时要求每一块都是合法的ip地址即可
+啥是合法的ip地址？
+首先如果是三位数
+必须大于0小于255
+只有一位数的开头第一位可以是0
+这就叫合法
+于是就有很多办法
+暴力遍历
+或者遍历以后在回溯
+很多方法
+
+
+总结地址：https://blog.csdn.net/qq_43491066/article/details/104277022
+ */
 
 public class RestoreIp {
 
