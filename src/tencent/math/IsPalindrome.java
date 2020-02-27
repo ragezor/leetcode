91c54812518b680d9d3892e54d3a08a91631609d
ragezor <457974608@qq.com>
2020-02-27 17:42:54 +0800

    feat: all the code of leetcode have update at 2020.2.27 本次更新了回文数

---
+++ src/tencent/math/IsPalindrome.java
@@ -0,0 +1,31 @@
+package tencent.math;
+import  java.util.*;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/104540968
+public class IsPalindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if ((x >= 0) && (x < 10)) {
+            return true;
+        } else {
+
+            int result = 0;
+            int tmp = x;
+            while (tmp > 0) {
+                result *= 10;
+                result += tmp%10;
+                tmp = tmp/10;
+            }
+            return result == x;
+        }
+
+        }
+
+
+        public static  void  main(String[] args){
+        IsPalindrome isPalindrome=new IsPalindrome();
+            System.out.println( isPalindrome.isPalindrome(10));
+        }
+
+
+}
