113af1aa5f7840f544218e8b447706c0b46ddb1a
ragezor <457974608@qq.com>
2020-04-09 12:39:14 +0800

    feat: all the code of leetcode have update at 2020.4.9本次更新了括号生成

---
+++ src/daily/GenerateParenthesis.java
@@ -0,0 +1,35 @@
+package daily;
+import  java.util.*;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/105407921
+public class GenerateParenthesis {
+    public List<String> generateParenthesis(int n) {
+        //res存储结果
+        List<String> res=new ArrayList<>();
+        dfs(n,n,"",res);
+        return  res;
+
+    }
+    //dfs递归函数
+    private void dfs(int left,int right,String  str,List<String> res){
+        //左右都没了 加入res
+        if(left==0&&right==0){
+            res.add(str);
+            return;
+        }
+        //还有左括号没有加 那就加
+        if(left>0){
+            dfs(left-1,right,str+"(",res);
+        }
+        //右括号一定在左括号后
+        if(right>left){
+            dfs(left,right-1,str+")",res);
+        }
+
+    }
+
+    public  static  void main(String[] args){
+        GenerateParenthesis  generateParenthesis=new GenerateParenthesis();
+        System.out.println(generateParenthesis.generateParenthesis(3));
+    }
+
+}
