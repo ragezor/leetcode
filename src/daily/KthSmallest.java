63db4341f9cc305e8a523a0a48fc7fd0db94f5f7
ragezor <457974608@qq.com>
2020-07-02 10:26:06 +0800

    feat: all the code of leetcode have update at 2020.7.2 更新了有序矩阵中第K小的元素

---
+++ src/daily/KthSmallest.java
@@ -0,0 +1,35 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/107078878
+public class KthSmallest {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+
+
+    }
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+        int i=n-1;
+        int j=0;
+        int sum=0;
+        while(i>=0&&j<n){
+            if(matrix[i][j]<=mid){
+                sum+=i+1;
+                j++;
+            }
+            else {
+                i--;
+            }
+        }
+        return  sum>=k;
+    }
+}
