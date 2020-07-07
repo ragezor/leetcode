729187f0b69fc10a34073ef4951964667f07bfe0
ragezor <457974608@qq.com>
2020-07-07 09:42:03 +0800

    feat: all the code of leetcode have update at 2020.7.7 更新了 路径总和

---
+++ src/daily/HasPathSum.java
@@ -0,0 +1,28 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/107174336
+public class HasPathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+
+
+}
