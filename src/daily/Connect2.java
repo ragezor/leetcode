3c32f7de8b7bd183c18ae30955dbd52f3f9f7567
ragezor <457974608@qq.com>
2020-10-15 08:29:16 +0800

    feat: all the code of leetcode have update at 2020.10.15更新了116. 填充每个节点的下一个右侧节点指针

---
+++ src/daily/Connect2.java
@@ -0,0 +1,31 @@
+package daily;
+
+import java.util.LinkedList;
+import java.util.Queue;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/109089755
+public class Connect2 {
+    public Node connect(Node root) {
+        if(root==null){
+            return  root;
+        }
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for (int i=0;i<size;i++){
+                Node node=queue.poll();
+                if(i<size-1){
+                    node.next=queue.peek();
+                }
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return  root;
+
+    }
+}
