0443fa374686ef21bfbcd9229c4d46f77ba34b3d
ragezor <457974608@qq.com>
2020-10-18 15:31:22 +0800

    feat: all the code of leetcode have update at 2020.10.8更新了删除链表的倒数第N个节点

---
+++ src/daily/RemoveNthFromEnd.java
@@ -0,0 +1,39 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/109145599
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode first=head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode second=dummy;
+        for (int i = 0; i <n ; ++i) {
+            first=first.next;
+
+        }
+        while(first!=null){
+            first=first.next;
+            second=second.next;
+        }
+        second.next=second.next.next;
+        return  dummy.next;
+
+
+    }
+
+    public static  void main(String[] args){
+        RemoveNthFromEnd removeNthFromEnd=new RemoveNthFromEnd();
+        ListNode head=new ListNode(1);
+        head.next=new ListNode(2);
+        head.next.next=new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        head.next.next.next.next=new ListNode(5);
+
+        ListNode ans=removeNthFromEnd.removeNthFromEnd(head,2);
+        while(ans!=null){
+            System.out.println(ans.val);
+            ans=ans.next;
+        }
+
+    }
+}
