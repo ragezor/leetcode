a7510164a41c31cf2a2986a67c8a941f552d9fc8
ragezor <457974608@qq.com>
2020-11-20 16:02:08 +0800

    feat: all the code of leetcode have update at 2020.11.20 更新了147. 对链表进行插入排序

---
+++ src/daily/InsertionSortList.java
@@ -0,0 +1,31 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/109852299
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+
+        if(head==null){
+            return  head;
+        }
+        ListNode dummyHead=new ListNode(0);
+        dummyHead.next=head;
+        ListNode last=head, curr=head.next;
+        while(curr!=null){
+            if(last.val<=curr.val){
+                last=last.next;
+            }
+            else {
+                ListNode pre=dummyHead;
+                while(pre.next.val<=curr.val){
+                    pre=pre.next;
+                }
+
+                last.next = curr.next;
+                curr.next = pre.next;
+                pre.next = curr;
+            }
+            curr = last.next;
+        }
+        return dummyHead.next;
+    }
+}
+
