17aa1bc5569855b132ab07a3763666ef7ccf7931
Ming963 <mxcnog@qq.com>
2020-12-04 22:00:42 +0800

    feat: all the code of leetcode have update at 2020.12.4 更新了 659.分割数组为连续子序列

---
+++ src/daily/SplitArrayintoConsecutiveSubsequences.java
@@ -0,0 +1,34 @@
+package daily;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class SplitArrayintoConsecutiveSubsequences {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<Integer, PriorityQueue<Integer>>();
+        for (int x : nums) {
+            if (!map.containsKey(x)) {
+                map.put(x, new PriorityQueue<Integer>());
+            }
+            if (map.containsKey(x - 1)) {
+                int prevLength = map.get(x - 1).poll();
+                if (map.get(x - 1).isEmpty()) {
+                    map.remove(x - 1);
+                }
+                map.get(x).offer(prevLength + 1);
+            } else {
+                map.get(x).offer(1);
+            }
+        }
+        Set<Map.Entry<Integer, PriorityQueue<Integer>>> entrySet = map.entrySet();
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : entrySet) {
+            PriorityQueue<Integer> queue = entry.getValue();
+            if (queue.peek() < 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
