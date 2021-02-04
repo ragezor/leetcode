82c3ce13fc636ca96a5cb5422467308773390902
ragezor <457974608@qq.com>
2021-02-04 19:56:11 +0800

    feat: all the code of leetcode have update at 2020.2.4新了子数组最大平均数 I

---
+++ src/daily/FindMaxAverage.java
@@ -0,0 +1,17 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/113663418
+public class FindMaxAverage {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return 1.0 * maxSum / k;
+    }
+}
