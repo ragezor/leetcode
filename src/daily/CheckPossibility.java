92520e6fa5789314e6df5ea9dcdf5b0b65fad031
ragezor <457974608@qq.com>
2021-02-07 18:48:59 +0800

    feat: all the code of leetcode have update at 2020.2.7更新了665. 非递减数列

---
+++ src/daily/CheckPossibility.java
@@ -0,0 +1,22 @@
+package daily;
+
+public class CheckPossibility {
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/113745758
+        public boolean checkPossibility(int[] nums) {
+            int n = nums.length, cnt = 0;
+            for (int i = 0; i < n - 1; ++i) {
+                int x = nums[i], y = nums[i + 1];
+                if (x > y) {
+                    cnt++;
+                    if (cnt > 1) {
+                        return false;
+                    }
+                    if (i > 0 && y < nums[i - 1]) {
+                        nums[i + 1] = x;
+                    }
+                }
+            }
+            return true;
+        }
+
+}
