f6eb13608de1460bdeb733cfad04e5b1273aaa36
ragezor <457974608@qq.com>
2021-02-17 20:31:11 +0800

    feat: all the code of leetcode have update at 2020.2.17更新了重塑矩阵

---
+++ src/daily/MatrixReshape.java
@@ -0,0 +1,17 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/113837707
+public class MatrixReshape {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+
+        int[][] ans = new int[r][c];
+        for (int x = 0; x < m * n; ++x) {
+            ans[x / c][x % c] = nums[x / n][x % n];
+        }
+        return ans;
+    }
+}
