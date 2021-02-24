bcbf8342d9063c64497d172bd5099794e06cc573
ragezor <457974608@qq.com>
2021-02-24 10:26:37 +0800

    feat: all the code of leetcode have update at 2020.2.24更新了832. 翻转图像

---
+++ src/daily/FlipAndInvertImage.java
@@ -0,0 +1,27 @@
+package daily;
+
+public class FlipAndInvertImage {
+    //总结地址：https://blog.csdn.net/qq_43491066/article/details/114012222
+
+        public int[][] flipAndInvertImage(int[][] A) {
+            int n = A.length;
+            for (int i = 0; i < n; i++) {
+                int left = 0, right = n - 1;
+                while (left < right) {
+                    if (A[i][left] == A[i][right]) {
+                        A[i][left] ^= 1;
+                        A[i][right] ^= 1;
+                    }
+                    left++;
+                    right--;
+                }
+                if (left == right) {
+                    A[i][left] ^= 1;
+                }
+            }
+            return A;
+        }
+    }
+
+
+
--- src/daily/IsToeplitzMatrix.java
+++ src/daily/IsToeplitzMatrix.java
@@ -11,6 +11,7 @@ public class IsToeplitzMatrix {
             }
         }
         return true;
+
     }
 
 
