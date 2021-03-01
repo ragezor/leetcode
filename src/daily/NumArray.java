43f20d35848d40c2fa2e0b30c1284bf92d1cd13c
ragezor <457974608@qq.com>
2021-03-01 16:12:47 +0800

    feat: all the code of leetcode have update at 2021.3.1更新了 区域和检索 - 数组不可变

---
+++ src/daily/NumArray.java
@@ -0,0 +1,17 @@
+package daily;
+//总结地址：https://blog.csdn.net/qq_43491066/article/details/114264986
+class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
