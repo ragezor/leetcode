40b139896b6830cf4fc8a5df64bb1cae20d3dc48
ragezor <457974608@qq.com>
2021-03-02 10:29:57 +0800

    feat: all the code of leetcode have update at 2021.3.2更新了304. 二维区域和检索 - 矩阵不可变

---
+++ src/daily/NumMatrix.java
@@ -0,0 +1,27 @@
+package daily;
+
+public class NumMatrix {
+    //总结地址：https://blog.csdn.net/qq_43491066/article/details/114278352
+    int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i][j + 1] = sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            sum += sums[i][col2 + 1] - sums[i][col1];
+        }
+        return sum;
+    }
+}
